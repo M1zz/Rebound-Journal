0454ecd8f620c46047b24afddc080c470ea208b7
HwangSukHyeon <payback9488@gmail.com>
2025-04-21 23:36:30 +0900

    [UI] 코드 일부 수정

--- Rebound Journal/Screen/JournalEntryItemView.swift
+++ Rebound Journal/Screen/JournalEntryItemView.swift
@@ -15,7 +15,7 @@ struct JournalEntryItemView: View {
             VStack(alignment: .leading, spacing: 10) {
                 JournalEntryHeaderView(model: model)
                 
-                if let entryText = model.moodText {
+                if let entryText = model.reasons {
                     Text(entryText)
                         .multilineTextAlignment(.leading)
                         .font(.system(size: 20, weight: .regular))
